430864077b594b43c8def6efd78d046fc66797a8
wayangalihpratama <wgprtm@gmail.com>
2025-05-06 11:54:29 +0800

    [#2] Sed routes with extra "/" to avoid redirecting

--- wgprtm-dev.sh
+++ wgprtm-dev.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+# sed routes with extra "/"
+sed -i.bak 's/@router\.post("\/",/@router.post("",/g; s/@router\.get("\/",/@router.get("",/g' ./backend/app/api/api_v1/chat.py
+sed -i.bak 's/@router\.post("\/",/@router.post("",/g; s/@router\.get("\/",/@router.get("",/g' ./backend/app/api/api_v1/api_keys.py
+
 COMPOSE_HTTP_TIMEOUT=180 docker compose \
   -f docker-compose-wgprtm.dev.yml \
   "$@"
