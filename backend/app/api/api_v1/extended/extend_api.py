f3b75e30a3f9e1a62eebef1483b7f66b6cb7ca7c
wayangalihpratama <wgprtm@gmail.com>
2025-09-30 15:40:33 +0800

    [#60] Remove extend_knowledge_base

--- backend/app/api/api_v1/extended/extend_api.py
+++ backend/app/api/api_v1/extended/extend_api.py
@@ -1,18 +1,12 @@
 from fastapi import APIRouter
 
 from app.api.api_v1.extended import (
-    extend_knowledge_base,
     prompt,
     system_settings,
 )
 
 api_router = APIRouter()
 
-api_router.include_router(
-    extend_knowledge_base.router,
-    prefix="/knowledge-base",
-    tags=["knowledge-base"],
-)
 api_router.include_router(prompt.router, prefix="/prompt", tags=["prompt"])
 api_router.include_router(
     system_settings.router,
