8dc0c75a1099e3a66f302fd7c3c85f0aa7cf1e30
wayangalihpratama <wgprtm@gmail.com>
2025-09-30 15:47:19 +0800

    [#60] Move out prompt & system_settings route from extended folder

--- backend/app/api/api_v1/api.py
+++ backend/app/api/api_v1/api.py
@@ -1,14 +1,37 @@
 from fastapi import APIRouter
-from app.api.api_v1 import auth, knowledge_base, chat, api_keys
-from app.api.api_v1.extended import extend_api
+from app.api.api_v1 import (
+    auth,
+    knowledge_base,
+    chat,
+    api_keys,
+    prompt,
+    system_settings
+)
+
 
 api_router = APIRouter()
 
-api_router.include_router(extend_api.api_router)
 
-api_router.include_router(auth.router, prefix="/auth", tags=["auth"])
-api_router.include_router(knowledge_base.router, prefix="/knowledge-base", tags=["knowledge-base"])
-api_router.include_router(chat.router, prefix="/chat", tags=["chat"])
-api_router.include_router(api_keys.router, prefix="/api-keys", tags=["api-keys"])
+api_router.include_router(
+    auth.router, prefix="/auth", tags=["auth"]
+)
 
-api_router.include_router(extend_api.api_router)
+api_router.include_router(
+    knowledge_base.router, prefix="/knowledge-base", tags=["knowledge-base"]
+)
+
+api_router.include_router(
+    chat.router, prefix="/chat", tags=["chat"]
+)
+
+api_router.include_router(
+    api_keys.router, prefix="/api-keys", tags=["api-keys"]
+)
+
+api_router.include_router(
+    prompt.router, prefix="/prompt", tags=["prompt"]
+)
+
+api_router.include_router(
+    system_settings.router, prefix="/system-settings", tags=["system-settings"]
+)
