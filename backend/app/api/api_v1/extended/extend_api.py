87424f003be7206f7f719f3ab420ae6e52e85259
wayangalihpratama <wgprtm@gmail.com>
2025-09-30 15:21:47 +0800

    [#60] Remove extend_api_keys

--- backend/app/api/api_v1/extended/extend_api.py
+++ backend/app/api/api_v1/extended/extend_api.py
@@ -1,7 +1,6 @@
 from fastapi import APIRouter
 
 from app.api.api_v1.extended import (
-    extend_api_keys,
     extend_chat,
     extend_auth,
     extend_knowledge_base,
@@ -18,9 +17,9 @@ api_router.include_router(
     tags=["knowledge-base"],
 )
 api_router.include_router(extend_chat.router, prefix="/chat", tags=["chat"])
-api_router.include_router(
-    extend_api_keys.router, prefix="/api-keys", tags=["api-keys"]
-)
 api_router.include_router(prompt.router, prefix="/prompt", tags=["prompt"])
-api_router.include_router(system_settings.router, prefix="/system-settings",
-                          tags=["system-settings"])
+api_router.include_router(
+    system_settings.router,
+    prefix="/system-settings",
+    tags=["system-settings"]
+)
